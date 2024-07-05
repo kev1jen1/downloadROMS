43894e207544964b6bc8e3ae3f9c20dc30f0e310
kev1jen1 <100167591+kev1jen1@users.noreply.github.com>
2024-07-05 14:18:25 +0200

    Add files via upload

--- rom_dowload.py
+++ rom_dowload.py
@@ -11,10 +11,10 @@ driver.get("https://www.romsgames.net/roms/nintendo/")
 
 driver.implicitly_wait(10)
 
-
+index = 0
 i = 0
 # Use index to keep track of the current element
-while i != 50:
+while index != 50:
     # Re-acquire the element list after navigation
     element = driver.find_element(By.TAG_NAME, 'section')
     elements = element.find_elements(By.TAG_NAME, "a")
@@ -32,14 +32,18 @@ while i != 50:
     if e.text == 'Next':
         print("nexteee")
         driver.execute_script("arguments[0].click();", e)
+        time.sleep(3)  # Pause to allow the next page to load
+        i = 0  # Reset the index to 0 to start over with the new elements
     else:
         # Click the element using JavaScript
         driver.execute_script("arguments[0].click();", e)
-        print(i)
+        print(index)
         i += 1
+        index += 1
         time.sleep(1)  # Pause to allow the page to load after click
         driver.back()
         time.sleep(1)  # Pause to allow the previous page to load
 
 time.sleep(100)
 driver.quit()
+
